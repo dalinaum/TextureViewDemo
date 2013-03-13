aad09273fa749e5e411501b4cc58f9a154d450e4
Leonardo YongUk Kim <dalinaum@gmail.com>
2013-03-14 04:45:30 +0900

    remove an unnecessary import

--- TextureViewExample/src/kr/gdg/android/textureview/ListActivity.java
+++ TextureViewExample/src/kr/gdg/android/textureview/ListActivity.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
